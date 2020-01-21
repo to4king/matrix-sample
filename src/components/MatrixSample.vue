55cc8b8383e5eb73cda4b52a2d04be280759b84c
to4king <to4king@gmail.com>
2020-01-22 01:31:41 +0900

    first commit.

---
+++ src/components/MatrixSample.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="area" v-on:click.once="getPosition()"></div>
+</template>
+
+<script>
+export default {
+  name: 'MatrixSample',
+  props: {
+    msg: String
+  },
+  methods: {
+    getPosition: function () {
+      document.addEventListener('mousemove', function(e) {
+        const _X = e.pageX;
+        const _Y = e.pageY;
+        alert(`X: ${_X}, Y: ${_Y}`);
+      });
+    },
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.area {
+  min-height: 50em;
+  min-width: 50em;
+  background-color: #777733;
+}
+</style>
