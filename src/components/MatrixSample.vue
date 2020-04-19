5609f6142001568feb471e25d801c2a1dd627731
to4king <to4king@gmail.com>
2020-04-20 01:45:26 +0900

    [WIP]change chart(quadrant scatter plot)

--- src/components/MatrixSample.vue
+++ src/components/MatrixSample.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="area" v-on:click.once="getPosition()">
+  <div v-on:click.once="getPosition()">
     <Chart
+      class="area"
       :chartData="this.chartData"
-      :options="this.options"
+      :chartOption="this.chartOption"
     />
   </div>
 </template>
@@ -10,15 +11,65 @@
 <script>
 import Chart from '../components/parts/Chart.vue';
 
-const CHART_DATA = [];
-const OPTIONS = [];
+const dataBgColors = [
+  '#f87979',
+  '#77f777',
+];
+const CHART_DATA = {
+  labels: ['January', 'Feburary'],
+  datasets: [
+    {
+      label: '社員A',
+      backgroundColor: dataBgColors[0],
+      data: [
+          {x:5, y:5},
+          {x:-5, y:-5},
+      ]
+    },
+    {
+      label: '社員B',
+      backgroundColor: dataBgColors[1],
+      data: [
+          {x:1, y:-3},
+          {x:2, y:2},
+      ]
+    },
+  ]
+};
+const CHART_OPTION = {
+  responsive: true,
+  scales: {
+    yAxes: [{
+      beginAtZero: false,
+      ticks: {
+        min: -5,
+        max: 5,
+        stepSize: 0.5
+      }
+    }],
+    xAxes: [{
+      beginAtZero: false,
+      ticks: {
+        min: -5,
+        max: 5,
+        stepSize: 0.5
+      }
+    }],
+  },
+  elements: {
+    point: {
+      radius: 6
+    }
+  },
+  labels: ['好み', '得意']
+}
 
 export default {
   name: 'MatrixSample',
   data: function() {
     return {
       chartData: CHART_DATA,
-      OPTIONS : OPTIONS,
+      chartOption: CHART_OPTION,
     };
   },
   components: {Chart},
@@ -27,7 +78,7 @@ export default {
       document.addEventListener('mousemove', function(e) {
         const _X = e.pageX;
         const _Y = e.pageY;
-        alert(`X: ${_X}, Y: ${_Y}`);
+        (`X: ${_X}, Y: ${_Y}`);
       });
     },
   }
@@ -37,8 +88,6 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .area {
-  min-height: 50em;
-  min-width: 50em;
-  background-color: #777733;
+  background-color: #eeeeee;
 }
 </style>
